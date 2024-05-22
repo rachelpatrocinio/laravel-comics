b50cbd42db9bea969d0ba68101fefbe087b963f9
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-22 14:41:32 +0200

    Added: new @yield on app.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,15 @@
 @section('title')
 <h1>HOME PAGE</h1>
 @endsection
+
 @section('content')
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae exercitationem explicabo debitis quia iste quis eius, sed ipsum. Mollitia quam ullam voluptatibus officia corporis odit molestiae non molestias asperiores vitae.</p>
+@endsection
+
+@section('comics-card')
+<div class="container">
+    <div class="row">
+        COMICS CARD
+    </div>
+</div>
 @endsection
