83c8e0e11bccb6abcaf675c2e314a45f9960ada9
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-22 18:10:04 +0200

    Added: layout on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,42 @@
-<footer>
-    <h1>FOOTER</h1>
+<footer class="main-footer pt-5 pb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <h6>DC COMICS</h6>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>                    
+                    <li>Movies</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <h6>DC COMICS</h6>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>                    
+                    <li>Movies</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>                
+            </div>
+            <div class="col-2">
+                <h6>DC COMICS</h6>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>                    
+                    <li>Movies</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>  
+            </div>
+            <div class="col-6">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png')}}" alt="">
+            </div>
+        </div>
+    </div>
 </footer>
